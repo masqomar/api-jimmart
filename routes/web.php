a8fe617b34cfa264fc55d53b52ba25ae1728cf7b
unknown <qomaruddin5@gmail.com>
2023-08-25 00:31:42 +0700

    tambah profil

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return [
-        'app' => 'Grocery Delivery API by initappz',
-        'version' => '1.0.0',
-    ];
+    return view('welcome');
 });
